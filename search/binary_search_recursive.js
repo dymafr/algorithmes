b0a7ac1c9c90d9fc6172b0247f1176661e13471d
dymafr <contact@dyma.fr>
2022-09-23 17:20:36 +0200

    binary searh recursive

--- search/binary_search_recursive.js
+++ search/binary_search_recursive.js
@@ -1,33 +1,28 @@
 import arrayGenerator from '../sort/arrayGenerator.js';
 
-// O(n) = log n
 function recursive_binary_search(
-  key,
-  sortedArray,
-  low = 0,
-  high = sortedArray.length - 1
+  recherche,
+  tableau,
+  debut = 0,
+  fin = tableau.length - 1
 ) {
-  if (low === high) {
-    if (sortedArray[low] === key) {
-      return low;
-    } else {
-      return null;
-    }
+  if (debut > fin) {
+    return null;
   } else {
-    const mid = Math.floor((low + high) / 2);
-    if (key === sortedArray[mid]) {
+    const mid = Math.floor((debut + fin) / 2);
+    if (tableau[mid] === recherche) {
       return mid;
-    } else if (key < sortedArray[mid]) {
-      return recursive_binary_search(key, sortedArray, low, mid - 1);
+    } else if (tableau[mid] > recherche) {
+      return recursive_binary_search(recherche, tableau, debut, mid - 1);
     } else {
-      return recursive_binary_search(key, sortedArray, mid + 1, high);
+      return recursive_binary_search(recherche, tableau, mid + 1, fin);
     }
   }
 }
 
-const sortedArray = arrayGenerator(20, 500).sort((a, b) => a - b);
-const key = sortedArray[Math.round(Math.random() * 19)];
-console.log('Tableau :', sortedArray);
-console.log('Elément à trouver :', key);
+const tableau = arrayGenerator(20, 500).sort((a, b) => a - b);
+const recherche = tableau[Math.round(Math.random() * 19)];
 
-console.log('Position :', recursive_binary_search(key, sortedArray));
+console.log('Tableau :', tableau);
+console.log('Elément à trouver :', recherche);
+console.log('Position :', recursive_binary_search(recherche, tableau));

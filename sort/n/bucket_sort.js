fb4c536941cd0a99a64b6923897ee05eb883df5d
dymafr <contact@dyma.fr>
2022-12-31 12:11:36 +0100

    bucket sort

--- sort/n/bucket_sort.js
+++ sort/n/bucket_sort.js
@@ -1,45 +1,75 @@
 import arrayGenerator from '../arrayGenerator.js';
 import { insertionSort } from '../n2/implementations/insertionSort.js';
 
-function createBuckets(arr, bucketSize, n) {
-  let minValue = arr[0];
-  let maxValue = arr[0];
-  for (let i = 1; i < n; i++) {
-    if (arr[i] < minValue) {
-      minValue = arr[i];
-    } else if (arr[i] > maxValue) {
-      maxValue = arr[i];
+function getMinMax(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
-  const bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+  return { min, max };
+}
+
+// Si on ne connait pas l'intervalle des valeurs :
+function bucketSort(arr) {
+  const { min, max } = getMinMax(arr);
+  const n = arr.length;
+  const h = (max - min) / n;
   const buckets = [];
-  for (let i = 0; i < bucketCount; i++) {
+  for (let i = 0; i < n; i++) {
     buckets[i] = [];
   }
   for (let i = 0; i < n; i++) {
-    buckets[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
-  }
-  return buckets;
-}
-
-function sortBuckets(buckets) {
-  const sortedArray = [];
-  for (let i = 0; i < buckets.length; i++) {
-    if (buckets[i] != null) {
-      insertionSort(buckets[i]);
-      sortedArray.push(...buckets[i]);
+    let k = 0;
+    while (min + h * (k + 1) < arr[i]) {
+      k++;
     }
+    buckets[k].push(arr[i]);
   }
-  return sortedArray;
+  console.log(buckets);
+  for (let i = 0; i < n; i++) {
+    insertionSort(buckets[i]);
+  }
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result = result.concat(buckets[i]);
+  }
+  return result;
 }
 
-export function bucketSort(arr, bucketSize = 5, n = arr.length) {
-  if (n < 2) {
-    return arr;
+// Si on connait l'intervalle des valeurs (ce qui est normalement le cas) :
+function bucketSortInterval(arr, min, max) {
+  const n = arr.length;
+  const h = (max - min) / n; // Calcul de l'intervalle des paquets, ici 10
+  const buckets = [];
+  for (let i = 0; i < n; i++) {
+    buckets[i] = [];
   }
-  const buckets = createBuckets(arr, bucketSize, n);
-  return sortBuckets(buckets);
+  for (let i = 0; i < n; i++) {
+    let k = 0;
+    // On calcule le paquet dans lequel on doit mettre la valeur :
+    while (min + h * (k + 1) < arr[i]) {
+      k++;
+    }
+    // On peut aussi faire :
+    // const k = Math.floor((arr[i] - min) / h);
+    buckets[k].push(arr[i]);
+  }
+  // On trie chaque paquet :
+  for (let i = 0; i < n; i++) {
+    insertionSort(buckets[i]);
+  }
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result = result.concat(buckets[i]);
+  }
+  return result;
 }
 
 const arr = arrayGenerator(20, 200);
-console.log(bucketSort(arr));
+console.log(bucketSort(arr, 0, 200));

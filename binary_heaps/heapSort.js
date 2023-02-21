dcd29d5063f3ee0193a3024b120c8dcf48703974
dymafr <contact@dyma.fr>
2023-02-21 12:37:35 +0100

    comment heapsort

--- binary_heaps/heapSort.js
+++ binary_heaps/heapSort.js
@@ -1,3 +1,4 @@
+// O(n log(n)) en temps | O(1) espace
 export default function heapSort(array) {
   buildMaxHeap(array);
   for (let i = array.length - 1; i > 0; i--) {
@@ -7,33 +8,36 @@ export default function heapSort(array) {
   return array;
 }
 
+// O(n) en temps | O(1) espace
 function buildMaxHeap(array) {
   for (let i = Math.floor(array.length / 2); i >= 0; i--) {
     heapifyDownRecursive(array, i);
   }
 }
 
+// O(1) en temps | O(1) espace
 function swap(array, indexA, indexB) {
   const temp = array[indexA];
   array[indexA] = array[indexB];
   array[indexB] = temp;
 }
 
-function heapifyDownRecursive(array, index, heapSize = array.length) {
+// O(log n) ou O(h) en temps | O(1) espace
+function heapifyDownRecursive(array, index, to = array.length) {
   let left = 2 * index + 1;
   let right = 2 * index + 2;
   let largest = index;
 
-  if (left < heapSize && array[left] > array[largest]) {
+  if (left < to && array[left] > array[largest]) {
     largest = left;
   }
 
-  if (right < heapSize && array[right] > array[largest]) {
+  if (right < to && array[right] > array[largest]) {
     largest = right;
   }
 
   if (largest !== index) {
     swap(array, index, largest);
-    heapifyDownRecursive(array, largest, heapSize);
+    heapifyDownRecursive(array, largest, to);
   }
 }

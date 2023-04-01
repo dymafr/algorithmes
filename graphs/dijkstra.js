7d483f1b2dcae981c1665239e911e373323ce9e2
dymafr <contact@dyma.fr>
2023-04-01 14:19:55 +0200

    dijkstra names

--- graphs/dijkstra.js
+++ graphs/dijkstra.js
@@ -1,42 +1,40 @@
 import MinPriorityQueue from '../binary_heaps/MinPriorityQueue.js';
 
-// Complexité temporelle : O((V + E) log V)
-// Complexité spatiale : O(V)
 export function dijkstraPriorityQueue(graph, startVertex, endVertex) {
   const distances = {};
   const previous = {}; // uniquement pour reconstruire le chemin
-  const queue = new MinPriorityQueue();
+  const priorityQueue = new MinPriorityQueue();
 
   for (const vertex in graph.adjacencyList) {
     if (vertex === startVertex) {
       distances[vertex] = 0;
-      queue.insert({ vertex, priority: 0 });
+      priorityQueue.insert({ vertex, priority: 0 });
     } else {
       distances[vertex] = Infinity;
     }
   }
 
-  while (!queue.isEmpty()) {
-    const { vertex } = queue.extractMin();
+  while (!priorityQueue.isEmpty()) {
+    const { vertex } = priorityQueue.extractMin();
     if (vertex === endVertex) {
-      break;
+      const path = [endVertex];
+      let previousVertex = previous[endVertex];
+      while (previousVertex) {
+        path.push(previousVertex);
+        previousVertex = previous[previousVertex];
+      }
+      return { path: path.reverse(), distance: distances[endVertex] };
     }
     for (const neighbor of graph.adjacencyList[vertex]) {
       const distance = distances[vertex] + neighbor.weight;
       if (distance < distances[neighbor.vertex]) {
         distances[neighbor.vertex] = distance;
         previous[neighbor.vertex] = vertex;
-        queue.insert({ vertex: neighbor.vertex, priority: distance });
+        priorityQueue.insert({ vertex: neighbor.vertex, priority: distance });
       }
     }
   }
 
-  // Reconstruire le chemin, on peut également retourner que la distance
-  const path = [endVertex];
-  let previousVertex = previous[endVertex];
-  while (previousVertex) {
-    path.push(previousVertex);
-    previousVertex = previous[previousVertex];
-  }
-  return { path: path.reverse(), distance: distances[endVertex] };
+  // Aucun chemin trouvé
+  return null;
 }
